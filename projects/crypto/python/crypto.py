52d6eec42541a9bcf46f0dfa044b17fc167f8a50
Go Tae Geon <gtg7784@lab-c.me>
2019-05-08 15:15:00 +0900

    asdf

--- projects/crypto/python/crypto.py
+++ projects/crypto/python/crypto.py
@@ -45,10 +45,10 @@
 import base64
 import hashlib
 import urllib.parse
-import Crypto
+
 from Crypto.PublicKey import RSA
-from Crypto import Random
-import ast
+from Crypto.Cipher import PKCS1_OAEP
+import zlib
 
 def printEmotion():
     print("                                                ,----,                                    ")
@@ -135,9 +135,93 @@ def printDecode():
     print("|   ,.'      |   | ,'   \\   \\ .'      `---`     |   ,.'      |   | ,'  |   |.'     ")
     print("'---'        `----'      `---`                  '---'        `----'    `---'       ")
 
+def generateKey():
+    #Generate a public/ private key pair using 4096 bits key length (512 bytes)
+    new_key = RSA.generate(4096, e=65537)
+
+    #The private key in PEM format
+    private_key = new_key.exportKey("PEM")
+
+    #The public key in PEM Format
+    public_key = new_key.publickey().exportKey("PEM")
+
+    print(private_key)
+    fd = open("private_key.pem", "wb")
+    fd.write(private_key)
+    fd.close()
+
+    print(public_key)
+    fd = open("public_key.pem", "wb")
+    fd.write("public key is ", public_key)
+    fd.close()
+
+
+def encrypt_rsa(blob, public_key):
+    #Import the Public Key and use for encryption using PKCS1_OAEP
+    rsa_key = RSA.importKey(public_key)
+    rsa_key = PKCS1_OAEP.new(rsa_key)
+
+    #compress the data first
+    blob = zlib.compress(blob)
+
+    #In determining the chunk size, determine the private key length used in bytes
+    #and subtract 42 bytes (when using PKCS1_OAEP). The data will be in encrypted
+    #in chunks
+    chunk_size = 470
+    offset = 0
+    end_loop = False
+    encrypted =  ""
+
+    while not end_loop:
+        #The chunk
+        chunk = blob[offset:offset + chunk_size]
+
+        #If the data chunk is less then the chunk size, then we need to add
+        #padding with " ". This indicates the we reached the end of the file
+        #so we end loop here
+        if len(chunk) % chunk_size != 0:
+            end_loop = True
+            chunk += " " * (chunk_size - len(chunk))
+
+        #Append the encrypted chunk to the overall encrypted file
+        encrypted += rsa_key.encrypt(chunk)
+
+        #Increase the offset by chunk size
+        offset += chunk_size
+
+    #Base 64 encode the encrypted file
+    return base64.b64encode(encrypted)
+
+def decrypt_rsa(encrypted_blob, private_key):
+
+    #Import the Private Key and use for decryption using PKCS1_OAEP
+    rsakey = RSA.importKey(private_key)
+    rsakey = PKCS1_OAEP.new(rsakey)
+
+    #Base 64 decode the data
+    encrypted_blob = base64.b64decode(encrypted_blob)
+
+    #In determining the chunk size, determine the private key length used in bytes.
+    #The data will be in decrypted in chunks
+    chunk_size = 512
+    offset = 0
+    decrypted = ""
+
+    #keep loop going as long as we have chunks to decrypt
+    while offset < len(encrypted_blob):
+        #The chunk
+        chunk = encrypted_blob[offset: offset + chunk_size]
+
+        #Append the decrypted chunk to the overall decrypted file
+        decrypted += rsakey.decrypt(chunk)
+
+        #Increase the offset by chunk size
+        offset += chunk_size
+
+    #return the decompressed decrypted data
+    return zlib.decompress(decrypted)
+
 def base(cyptotype):
-    print("base64 type is", end=" ")
-    print(cyptotype)
     if (cyptotype == 1):
         inp = input("Type you want to encode: ")
         a = inp.encode("utf-8")
@@ -165,18 +249,39 @@ def url(cyptotype):
     
 def rsa(cyptotype):
     if (cyptotype == 1):
-        random_generator = Random.new().read
-        key = RSA.generate(1024, random_generator) #generate pub and priv key
+        inp = input("Type you want to encode: ")
+        unencrypted_blob = inp.encode("utf-8")
 
-        publickey = key.publickey() # pub key export for exchange
+        generateKey()
 
-        encrypted = publickey.encrypt('encrypt this message', 32)
-        #message to encrypt is in the above line 'encrypt this message'
+        fd = open("public_key.pem", "rb")
+        public_key = fd.read()
+        fd.close()
 
-        print ('encrypted message:', encrypted) #ciphertext
-        f = open ('encryption.txt', 'w')
-        f.write(str(encrypted)) #write ciphertext to file
-        f.close()
+        encrypted_blob = encrypt_rsa(unencrypted_blob, public_key)
+
+        fd = open("encrypted.txt", "wb")
+        fd.write(encrypted_blob.encode('utf-8'))
+        fd.close()
+
+
+    elif (cyptotype == 2):  
+        inp = input("Type you want to encode: ")
+        a = inp.encode("utf-8")
+
+        fd = open("private_key.pem", "rb")
+        private_key = fd.read()
+        fd.close()
+
+        #Our candidate file to be decrypted
+        fd = open("encrypted.txt", "rb")
+        encrypted_blob = fd.read()
+        fd.close()
+
+        #Write the decrypted contents to a file
+        fd = open("decrypted.txt", "wb")
+        fd.write(decrypt_rsa(encrypted_blob, private_key))
+        fd.close()
 
         
 
